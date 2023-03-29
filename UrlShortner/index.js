01deb9e4ea6f3a9512dc783dfa601995a4176599
Mukama Steven <stevenmukama1@gmail.com>
2023-03-29 08:00:07 +0200

    add urlshortner

---
+++ UrlShortner/index.js
@@ -0,0 +1,29 @@
+const urls = [];
+
+const shortenUrl = (url) => {
+  const shortUrl = `https://short.micro/${Math.random()
+    .toString(36)
+    .substring(2, 7)}`;
+  urls.push({ originalUrl: url, shortUrl });
+  console.log(
+    shortUrl,
+    "This is the shortened url, copy it to visit the original url"
+  );
+  return shortUrl;
+};
+
+shortenUrl(
+  "https://learn.microsoft.com/en-us/dotnet/csharp/?WT.mc_id=dotnet-35129-website"
+);
+
+const visitUrl = (shortUrl) => {
+  const url = urls.find((url) => url.shortUrl === shortUrl);
+  if (!url) {
+    return "Url not found";
+  }
+  return url.originalUrl;
+};
+console.log(
+  "This is what you'll get when you enter short url",
+  visitUrl(urls[urls.length - 1].shortUrl)
+);
